8004955575e8bb3d0d23cca868fa5445f0fd376f
devmanagement23 <devmanagement23@gmail.com>
2023-07-26 17:17:26 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    print('''
+            1.OPEN NEW ACCOUNT
+            2.DEPOSITE AMOUNT
+            3.WITHDRAW AMOUNT
+            4.BALANCE ENQUIRY
+            5.DISPLAY CUSTOMER DETAILS
+            6.CLOSE AN ACCOUNT
+    ''')
+
+    choice = input("Enter The Task You Want To Perform  ")
+    
+    if(choice=='1'):
+        OpenAccount()
+    elif(choice=='2'):
+        DepositeAmount()
+    elif(choice=='3'):
+        WithdrawAmount()
+    elif(choice=='4'):
+        BalanceEnquiry()
+    elif(choice=='5'):
+        DisplayDetails()
+    elif(choice=='6'):
+        CloseAccount()
+    else:
+        print("Invalid Choice")
+        main()
+
+main()
